5765378730fc904b82cbda1a1599bceca8ccf6b3
Alina <alina.mikhedkina@gmail.com>
2023-10-16 17:48:25 +0200

    frames v 3

--- tests/frames.test.ts
+++ tests/frames.test.ts
@@ -17,7 +17,47 @@ test("Interact with frames",async ({page}) => {
     await frame1?.fill("//label[text()='Last Name']/following::input", "Ltest")
 
     await page.waitForTimeout(3000);
-    expect(frame1?.locator("//p[text()='You have entered test test']")).toBeFalsy
+    expect(frame1?.locator("//p[@class='title has-text-info']").textContent()).toContain("You have entered") //doesn't work properly
+
 
     
+})
+
+test("Interact with frames. v2",async ({page}) => {
+
+    await page.goto("https://letcode.in/frame");
+
+    await page.locator("(//p[@class='fc-button-label'])[1]").click(); //consent agree
+
+    const allFrames = page.frames();
+    console.log("no of frames "+ allFrames.length);
+
+    const frame =  page.frameLocator("#firstFr")
+    await frame.locator("input[name='fname']").fill("Test")
+    await frame.locator("input[name='lname']").fill("Test 2");
+    await page.waitForTimeout(3000);
+
+    expect(frame?.locator("p.has-text-info").textContent()).toContain("You have entered") //doesn't work properly
+
+})
+
+test("Nested frames",async ({page}) => {   //also doesn't work properly
+
+    await page.goto("https://letcode.in/frame");
+
+    await page.locator("(//p[@class='fc-button-label'])[1]").click(); //consent agree
+
+    const allFrames = page.frames();
+    console.log("no of frames "+ allFrames.length);
+    
+    const frame =  page.frameLocator("#firstFr")
+
+    const innerFrame = frame.frameLocator("iframe[scr='innerFrame']")
+    await innerFrame.locator("input[name='email']").fill('email@gmail.com')
+   
+
+    await page.waitForTimeout(3000);
+
+    // expect(innerframe.locator("p.has-text-info").textContent()).toContain("You have entered") //doesn't work properly
+
 })
